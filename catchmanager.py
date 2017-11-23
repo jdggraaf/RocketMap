0b2519ae1e5751e0bc3fe4e9c443ef51ed9b835e
ShuckleHQ <8nmojd+l1dqz2bvl@sharklasers.com>
2017-11-23 14:31:47 +0100

    disabled 50 due to ball situation

--- catchmanager.py
+++ catchmanager.py
@@ -45,7 +45,7 @@ class CatchConditions:
         result.only_candy = True
         result.only_candy_12 = True
         result.only_unseen = True
-        result.catch_50 = True
+        result.catch_50 = False
         return result
 
     def is_candy_pokemon(self, pokemon_id):
