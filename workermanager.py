3e60ffdcac03acc2a58f919e2287753adc5e9ea6
ShuckleHQ <8nmojd+l1dqz2bvl@sharklasers.com>
2017-11-28 06:26:16 +0100

    Skipped itertols

--- workermanager.py
+++ workermanager.py
@@ -65,16 +65,14 @@ class WorkerManager(object):
         # did_map_objects = True
 
     def register_xp(self, xp):
-        self.xp_log.append((datetime.now, xp))
+        self.xp_log.append((datetime.now(), xp))
 
     def xp_30_minutes_ago(self):
         cutoff = datetime.now() - timedelta(minutes=30)
         best = None
-        for log_tuple in reversed(self.xp_log):
-            if cutoff <= log_tuple[0]:
-                best = log_tuple[1]
-            else:
-                return best
+        within_timeframe = dropwhile(lambda log_tuple_: cutoff >= log_tuple_[0], self.xp_log)
+        self.xp_log = within_timeframe
+        best = self.xp_log[0]
         return best
 
     def reached_target_level(self):
